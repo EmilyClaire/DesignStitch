3b6f9ef906e72a4a9a3f5b3f798f628be6770da5
Emily Claire Gok Lucas <eclucas@calpoly.edu>
2015-11-01 03:14:57 -0800

    I think it works for touchscreens now...

--- mvc.js
+++ mvc.js
@@ -18,12 +18,12 @@ base.strokeStyle = '#BDBDBD';
 
 //A method that draws the grid background
 function drawAida() {
-  var height = Math.floor(canvasBase.height/stitchSide) * stitchSide;
-  var width = Math.floor(canvasBase.width/stitchSide) * stitchSide;
+    var height = Math.floor(canvasBase.height / stitchSide) * stitchSide,
+        width = Math.floor(canvasBase.width / stitchSide) * stitchSide;
 
-  //Making the background a
-  base.fillStyle = '#F5F6CE';
-  base.fillRect(0, 0, width, height);
+        //Making the background a
+    base.fillStyle = '#F5F6CE';
+    base.fillRect(0, 0, width, height);
 
     for (var i = 0; i * stitchSide <= width + stitchSide; i ++){
         base.beginPath();
@@ -54,7 +54,7 @@ var mouseDown = false;
 var isDrawing = false;
 
 canvas.addEventListener("mousedown", function(event){printStitch(event.pageX, event.pageY);
-      mouseDown = true;}, false);
+      mouseDown = true; event.preventDefault();}, false);
 
 canvas.addEventListener("mousemove", function(event){  if(mouseDown){
       printStitch(event.pageX, event.pageY);}}, false);
@@ -62,20 +62,29 @@ canvas.addEventListener("mousemove", function(event){  if(mouseDown){
 canvas.addEventListener("mouseup",   function(){mouseDown = false;}
 , false);
 
-canvas.addEventListener("touchstart", function(event){
-        isDrawing = true;
+canvas.addEventListener('touchstart', function(event){
+        event.preventDefault();
+        printStitch(event.changedTouches[0].clientX, event.changedTouches[0].clientY);
+        console.log("start");
     }, false);
 
 canvas.addEventListener('touchmove', function(event){
-      if(isDrawing){
-        printStitch(event.changedTouches[event.changedTouches.length - 1].clientX, event.changedTouches[event.changedTouches.length - 1].clientY);
-        event.preventDefault();
-    }}, false)
+          event.preventDefault();
+          changes = event.changedTouches;
+          if(changes.length > 1){
+            printStitch(changes[changes.length - 1].clientX, changes[changes.length - 1].clientY);
+          }
+          else {
+            printStitch(changes[0].clientX, changes[0].clientY);
+          }
 
-
-canvas.addEventListener("touchend", function(event){
-    isDrawing = false;
+          console.log("moving");
     }, false);
+
+canvas.addEventListener('touchend', function(event){event.preventDefault(); console.log("end");}, false);
+
+canvas.addEventListener('touchcancel', function(event){event.preventDefault(); console.log("cancel");}, false);
+
 
 //Checks to make sure the x is in the bounds
 function boundsCheck(x, y)
